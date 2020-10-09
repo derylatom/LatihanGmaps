d24f51a13eeb174060c6b7495a520dd69805f802
derylatom <dery037yj@gmail.com>
2020-10-09 22:53:42 +0800

    first commit

---
+++ app/src/main/java/com/example/latihangmaps/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.latihangmaps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btGetLocationBaseServices = findViewById(R.id.btLBS);
+
+        btGetLocationBaseServices.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MapsActivity.class);
+            startActivity(intent);
+        });
+    }
+}
